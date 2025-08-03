0b90f8c5602691ac20e834941d22beb2b909516d
Kenneth <kenwillg85@gmail.com>
2025-08-03 18:42:16 +0700

    pertemuan 3 done

---
+++ pertemuan3/index.php
@@ -0,0 +1,42 @@
+<?php
+
+// Control Flow!!
+
+// for
+echo "For Loop: <br>";
+for ($i = 0; $i < 5; $i++) {
+    echo "Hello World (for) <br>";
+}
+
+echo "<br>";
+
+// while
+echo "While Loop: <br>";
+$i = 0; // Re-assign
+
+while ($i < 5) {
+    echo "Hello World (while) <br>";
+    $i++;
+}
+
+echo "<br>";
+
+// do-while
+echo "Do-while Loop: <br>";
+$i = 0; //Re-assign
+
+do {
+    echo "Hello World (do) <br>";
+    $i++;
+} while ($i < 5);
+
+
+// foreach
+
+// if
+
+// if-else
+
+// ternary
+
+// switch
---
+++ pertemuan3/latihan2.php
@@ -0,0 +1,14 @@
+<?php
+// Conditionals
+
+// if
+$x = 10;
+if ($x < 20) {
+    echo "Yes, $x is lesser than 20";
+}
+echo "<br> tetep dijalankan meski benar/salah";
+// if else
+
+// ternary
+
+// switch
